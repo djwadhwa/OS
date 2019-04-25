a575bed95582483afe0a68816df986ecd5c5ca9d
Stuffdude99 <wadhwadhananjay@gmail.com>
2019-04-25 15:02:44 -0700

    cleaned up code

--- DateServer.java
+++ DateServer.java
@@ -1,41 +1,33 @@
 import java.net.*;
 import java.io.*;
 
-public class DateServer extends Thread{
+public class DateServer extends Thread {
 	Socket client;
-	public DateServer(Socket client) {
-		// TODO Auto-generated constructor stub
-		this.client =client;
+
+	public DateServer(Socket c) {
+		client = c;
 	}
-	public void run()
-	{
-//		System.out.println("Thread #:" + Thread.currentThread().getId()+
-//				" Date:"+new java.util.Date().toString());
+	
+	public void run() {
 		try {
-			PrintWriter pout = new
-			PrintWriter(client.getOutputStream(), true);
+			PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
 			/* write the Date to the socket */
 			pout.println(new java.util.Date().toString());
 			/* close the socket and resume */
 			/* listening for connections */
 			client.close();
-		}
-		catch (IOException ioe) {
+		} catch (IOException ioe) {
 			System.err.println(ioe);
 		}
+
 	}
-	public static void main(String[] args) throws InterruptedException {
-		
+
+	public static void main(String[] args) {
 		try {
 			ServerSocket sock = new ServerSocket(6013);
-			/* now listen for connections */
-			while (true) {
-			Socket client = sock.accept();
-			DateServer d = new DateServer (client);
-			d.start();
-			}
-		}
-		catch (IOException ioe) {
+			DateServer ds = new DateServer(sock.accept());
+			ds.start();
+		} catch (IOException ioe) {
 			System.err.println(ioe);
 		}
 	}
